9339f19fb093b1f0065f6a30e8d7a26607d8cc26
Kevin Lee <kevlee2198@gmail.com>
2025-12-27 21:33:51 -0800

    better-auth setup

--- README.md
+++ README.md
@@ -34,3 +34,7 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+## Authentication
+
+This project uses [better-auth](https://www.better-auth.com/) to handle authentication. It uses keycloak as the IDP and better auth as a stateless management system.
---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import { betterAuth } from "better-auth";
+import { genericOAuth } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 7 * 24 * 60 * 60, // 7 days cache duration
+      strategy: "jwe", // can be "jwt" or "compact"
+      refreshCache: true, // Enable stateless refresh
+    },
+  },
+  account: {
+    storeStateStrategy: "cookie",
+    storeAccountCookie: true, // Store account data after OAuth flow in a cookie (useful for database-less flows)
+  },
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: "keycloak",
+          clientId: process.env.KEYCLOAK_CLIENT_ID as string,
+          clientSecret: process.env.KEYCLOAK_CLIENT_SECRET as string,
+          discoveryUrl: process.env.KEYCLOAK_DISCOVERY_URL as string,
+          // "https://auth.example.com/.well-known/openid-configuration",
+        },
+      ],
+    }),
+  ],
+});
